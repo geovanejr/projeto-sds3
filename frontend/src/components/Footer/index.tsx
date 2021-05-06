1f51e1f67b2ac8648419effb439561eea234ebe2
Geovane Junior <geovane.gjunior@gmail.com>
2021-05-06 08:23:20 -0300

    Static charts

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,16 +1,16 @@
 import { ReactComponent as LinkedinIcon } from 'assets/img/linkedin.svg';
-import { ReactComponent as GithubIcon} from 'assets/img/github.svg';
+import { ReactComponent as GithubIcon } from 'assets/img/github.svg';
 
 const Footer = () => {
     return (
         // <footer className="main-footer py-3 bg-dark">
         // <footer className="main-footer bg-dark">
-        <footer className="footer mt-auto py-3 bg-dark">
+        <footer className="footer mt-auto py-1 bg-dark">
             <div className="container">
                 <p className="text-light">App desenvolvido por <a href="https://https://github.com/geovanejr" target="_blank" rel="noreferrer">Geovane Junior</a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
       Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
-      <a href="https://www.linkedin.com/in/geovane-junior-7333412/" target="_new">
+                <a className="icon-footer-Lkd px-2" href="https://www.linkedin.com/in/geovane-junior-7333412/" target="_new">
                     <LinkedinIcon />
                 </a>
                 <a href="https://github.com/geovanejr" target="_new">
